0d568a58e21447fce456dbc4c54dc9085afcdfb7
Daana <126dpelaezayala@frhsd.com>
2025-11-20 08:16:06 -0500

    message

---
+++ js/js.js
@@ -0,0 +1,6 @@
+const secretParagraph = document.getElementById('secretText991');
+
+    
+        secretParagraph.style.visibility = 'visible';
+
+        secretParagraph.textContent = 'You found me using the DOM!';
