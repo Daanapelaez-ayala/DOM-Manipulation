9eff7ec308f84cca692a2e36a6a03841ce08059c
Daana <126dpelaezayala@frhsd.com>
2025-11-25 08:05:31 -0500

    Message

--- js/js.js
+++ js/js.js
@@ -15,7 +15,7 @@ box7.addEventListener('mouseover', border-radius())
 function border-radius() {
   box7.style.borderRadius = '25px';
 }
-box7.addEventListener('mouseout', normalBorder())
+box7.addEventListener('dblclick', normalBorder)
 function normalBorder() {
   box7.style.borderRadius = '0px';
 }
