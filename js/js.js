96a36c841b498a3b4e09764bd9d26bcc42b1dc09
Daana <126dpelaezayala@frhsd.com>
2025-11-25 08:24:43 -0500

    Message

--- js/js.js
+++ js/js.js
@@ -5,12 +5,13 @@ const secretParagraph = document.getElementById('secretText991');
 
         secretParagraph.textContent = 'You found me using the DOM!';
 
+// Task 2: Button behavior
 const weirdButtonX = document.getElementById('weirdButtonX');
 weirdButtonX.addEventListener('click', handleClick);
 function handleClick() {
   alert('Button clicked!');
 }
-
+// Task 3: Border Radius Change
 const box7 = document.getElementById('box7');
 box7.addEventListener('mouseover', borderRadius)
 function borderRadius() {
@@ -20,7 +21,7 @@ box7.addEventListener('dblclick', normalBorder)
 function normalBorder() {
   box7.style.borderRadius = '0px';
 }
-
+// Task 4: Decode Message
 const decodedOutput = document.getElementById('decodedOutput');
 function decodeMessage() {
 //   const codedMessage = document.getElementById('codedMessage').value;
@@ -29,3 +30,10 @@ function decodeMessage() {
         decodedOutput.textContent = decodedMessage;
         return;
         }
+// Task 5: class switch
+const mysterylist = document.getElementById('mysteryList');
+mysterylist.addEventListener('click', changeColor);
+let highlight = yellow;
+if (mysterylist.classList.contains('ul')) {
+    highlight = yellow;
+}
